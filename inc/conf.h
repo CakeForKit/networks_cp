51d13b4974f79821c0fbeadfc6c1abe631cdaa38
KathrineWSL <c.k4terin4@mail.ru>
2025-12-13 20:43:02 +0300

    -

--- inc/conf.h
+++
@@ -1,7 +0,0 @@
-#define MAX_CONNECTIONS 1024
-#define MAX_WORKERS 4
-#define BUFFER_SIZE 8192
-#define MAX_PATH_LENGTH 2048
-#define LOG_FILE "./logs/server.log"
-#define PORT 8080
-#define MAX_FILE_SIZE_BYTES 128 * 1024 * 1024 // 128 Мб
--- inc/logs.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void log_message(const char *message);
-void log_request(const char *method, const char *path, int status);
